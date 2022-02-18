7713a2ac005cde8afa1d9cd4e21400610b61c879
bonifasiustrg <bonifasius.tambak@gmail.com>
2022-02-18 13:25:30 +0700

    Upload1

---
+++ app/src/main/java/id/com/waluya/konsultasi_dokter.kt
@@ -0,0 +1,11 @@
+package id.com.waluya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class konsultasi_dokter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_konsultasi_dokter)
+    }
+}
